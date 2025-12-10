d27f5cdbedca930d75d797c381126c5eb12fc6b1
ltcucptdtbtthtsvixuyen-star <ltcuc.ptdtbtthtsvixuyen@hagiang.edu.vn>
2025-12-10 10:50:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lê Thị Cúc - Trường PTDTBT Tiểu học Thượng Sơn - ĐT: 0984048092</div>", unsafe_allow_html=True)
